129a633256ee2acebe3ab0ee79fa91d68b519497
Taivasvalas <kuusi.akseli@gmail.com>
2025-04-19 01:11:50 +0300

    Use ngrok URLs in production for frontend

--- Marketplace 12 (Making 11 work again)/frontend/my-react-app/src/BaseUrl.js
+++ Marketplace 12 (Making 11 work again)/frontend/my-react-app/src/BaseUrl.js
@@ -3,5 +3,22 @@
 // export const BASE_URL81 = "http://localhost:8081";
 
 
-export const BASE_URL80 = "/api80";
-export const BASE_URL81 = "/api81";
+// export const BASE_URL80 = "/api80";
+// export const BASE_URL81 = "/api81";
+
+
+
+// Kun kehität lokaalisti, frontendi käyttää setupProxy.js:ssä määriteltyjä proxypolkuja.
+// Kun projekti on buildattu Verceliin, se käyttää suoraan julkisia ngrok-URL:eja.
+
+
+
+const isProd = process.env.NODE_ENV === "production";
+
+export const BASE_URL80 = isProd
+  ? "https://d73f-194-197-64-102.ngrok-free.app"
+  : "/api80";
+
+export const BASE_URL81 = isProd
+  ? "https://1dd6-194-197-64-102.ngrok-free.app"
+  : "/api81";
